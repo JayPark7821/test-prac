105d26d6dd48e58a4ba15fdc1bb692e98f6ee1a1
jaypark <pjhyun1990@gmail.com>
2023-06-15 22:12:12 +0900

    UseCase 분리

--- src/main/java/com/example/demo/user/controller/UserController.java
+++ src/main/java/com/example/demo/user/controller/UserController.java
@@ -1,17 +1,7 @@
 package com.example.demo.user.controller;
 
-import com.example.demo.user.controller.port.UserService;
-import com.example.demo.user.controller.response.MyProfileResponse;
-import com.example.demo.user.controller.response.UserResponse;
-import com.example.demo.user.domain.User;
-import com.example.demo.user.domain.UserUpdate;
-import com.example.demo.user.service.UserServiceImpl;
-
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import java.net.URI;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,58 +14,71 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.user.controller.port.AuthenticationService;
+import com.example.demo.user.controller.port.UserReadService;
+import com.example.demo.user.controller.port.UserUpdateService;
+import com.example.demo.user.controller.response.MyProfileResponse;
+import com.example.demo.user.controller.response.UserResponse;
+import com.example.demo.user.domain.User;
+import com.example.demo.user.domain.UserUpdate;
+
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
 @Tag(name = "유저(users)")
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserService userService;
+	private final UserReadService userReadService;
+	private final UserUpdateService userUpdateService;
+	private final AuthenticationService authenticationService;
 
-    @ResponseStatus
-    @GetMapping("/{id}")
-    public ResponseEntity<UserResponse> getUserById(@PathVariable long id) {
-        return ResponseEntity
-            .ok()
-            .body(UserResponse.from(userService.getById(id)));
-    }
+	@ResponseStatus
+	@GetMapping("/{id}")
+	public ResponseEntity<UserResponse> getUserById(@PathVariable long id) {
+		return ResponseEntity
+			.ok()
+			.body(UserResponse.from(userReadService.getById(id)));
+	}
 
-    @GetMapping("/{id}/verify")
-    public ResponseEntity<Void> verifyEmail(
-        @PathVariable long id,
-        @RequestParam String certificationCode) {
-        userService.verifyEmail(id, certificationCode);
-        return ResponseEntity.status(HttpStatus.FOUND)
-            .location(URI.create("http://localhost:3000"))
-            .build();
-    }
+	@GetMapping("/{id}/verify")
+	public ResponseEntity<Void> verifyEmail(
+		@PathVariable long id,
+		@RequestParam String certificationCode) {
+		authenticationService.verifyEmail(id, certificationCode);
+		return ResponseEntity.status(HttpStatus.FOUND)
+			.location(URI.create("http://localhost:3000"))
+			.build();
+	}
 
-    @GetMapping("/me")
-    public ResponseEntity<MyProfileResponse> getMyInfo(
-        @Parameter(name = "EMAIL", in = ParameterIn.HEADER)
-        @RequestHeader("EMAIL") String email // 일반적으로 스프링 시큐리티를 사용한다면 UserPrincipal 에서 가져옵니다.
-    ) {
-        User user = userService.getByEmail(email);
-        userService.login(user.getId());
-        return ResponseEntity
-            .ok()
-            .body(MyProfileResponse.from(user));
-    }
+	@GetMapping("/me")
+	public ResponseEntity<MyProfileResponse> getMyInfo(
+		@Parameter(name = "EMAIL", in = ParameterIn.HEADER)
+		@RequestHeader("EMAIL") String email // 일반적으로 스프링 시큐리티를 사용한다면 UserPrincipal 에서 가져옵니다.
+	) {
+		User user = userReadService.getByEmail(email);
+		authenticationService.login(user.getId());
+		return ResponseEntity
+			.ok()
+			.body(MyProfileResponse.from(user));
+	}
 
-    @PutMapping("/me")
-    @Parameter(in = ParameterIn.HEADER, name = "EMAIL")
-    public ResponseEntity<MyProfileResponse> updateMyInfo(
-        @Parameter(name = "EMAIL", in = ParameterIn.HEADER)
-        @RequestHeader("EMAIL") String email, // 일반적으로 스프링 시큐리티를 사용한다면 UserPrincipal 에서 가져옵니다.
-        @RequestBody UserUpdate userUpdate
-    ) {
-        User user = userService.getByEmail(email);
-        user = userService.update(user.getId(), userUpdate);
-        return ResponseEntity
-            .ok()
-            .body(MyProfileResponse.from(user));
-    }
-
-
+	@PutMapping("/me")
+	@Parameter(in = ParameterIn.HEADER, name = "EMAIL")
+	public ResponseEntity<MyProfileResponse> updateMyInfo(
+		@Parameter(name = "EMAIL", in = ParameterIn.HEADER)
+		@RequestHeader("EMAIL") String email, // 일반적으로 스프링 시큐리티를 사용한다면 UserPrincipal 에서 가져옵니다.
+		@RequestBody UserUpdate userUpdate
+	) {
+		User user = userReadService.getByEmail(email);
+		user = userUpdateService.update(user.getId(), userUpdate);
+		return ResponseEntity
+			.ok()
+			.body(MyProfileResponse.from(user));
+	}
 
 }
