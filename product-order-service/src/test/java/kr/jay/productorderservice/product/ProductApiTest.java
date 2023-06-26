1252e35014aa270105416e95b8bb4217c578fd6b
jaypark <pjhyun1990@gmail.com>
2023-06-26 16:30:38 +0900

    ProductService::getProduct rest-assured 테스트로 변경

--- product-order-service/src/test/java/kr/jay/productorderservice/product/ProductApiTest.java
+++ product-order-service/src/test/java/kr/jay/productorderservice/product/ProductApiTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import kr.jay.productorderservice.ApiTest;
 
 /**
@@ -31,4 +34,18 @@ class ProductApiTest extends ApiTest {
 
 	}
 
+	@Test
+	void 상품조회() {
+		ProductSteps.상품등록요청(ProductSteps.상품등록요청_생성());
+		final Long productId = 1L;
+
+		final ExtractableResponse<Response> response = RestAssured.given().log().all()
+			.when()
+			.get("/products/{productId}", productId)
+			.then().log().all()
+			.extract();
+
+		assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+		assertThat(response.jsonPath().getString("name")).isEqualTo("상품명");
+	}
 }
