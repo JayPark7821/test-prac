36c5e877d83fce42e653197c8f89a8138492227b
jaypark <pjhyun1990@gmail.com>
2023-06-26 17:49:46 +0900

    ProductService::updateProduct springBootTest로 변경

--- product-order-service/src/test/java/kr/jay/productorderservice/product/ProductServiceTest.java
+++ product-order-service/src/test/java/kr/jay/productorderservice/product/ProductServiceTest.java
@@ -1,9 +1,11 @@
 package kr.jay.productorderservice.product;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 /**
  * ProductServiceTest
@@ -12,40 +14,24 @@ import org.junit.jupiter.api.Test;
  * @version 1.0.0
  * @since 2023/06/26
  */
+@SpringBootTest
 class ProductServiceTest {
 
+	@Autowired
 	private ProductService productService;
-	private StubProductPort productPort = new StubProductPort();
-
-	@BeforeEach
-	void setUp() {
-		productService = new ProductService(productPort);
-	}
 
 	@Test
 	void 상품수정() {
+		productService.addProduct(ProductSteps.상품등록요청_생성());
 		final Long productId = 1L;
 		final UpdateProductRequest request = new UpdateProductRequest("상품 수정", 20000, DiscountPolicy.NONE);
-		final Product product = new Product("상품명", 10000, DiscountPolicy.NONE);
-		productPort.getProduct_will_return = product;
+
 		productService.updateProduct(productId, request);
 
-		assertThat(product.getProductName()).isEqualTo("상품 수정");
-		assertThat(product.getPrice()).isEqualTo(20000);
+		final ResponseEntity<GetProductResponse> response = productService.getProduct(productId);
+		final GetProductResponse productResponse = response.getBody();
+		assertThat(productResponse.name()).isEqualTo("상품 수정");
+		assertThat(productResponse.price()).isEqualTo(20000);
 	}
 
-	private static class StubProductPort implements ProductPort {
-
-		public Product getProduct_will_return;
-
-		@Override
-		public void save(final Product product) {
-
-		}
-
-		@Override
-		public Product getProduct(final Long productId) {
-			return getProduct_will_return;
-		}
-	}
 }
